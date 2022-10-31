1c11e347fece0638f88aa4b38ba5cd98c0ca1e17
Adela <adela.karsznia@hotmail.com>
2022-10-31 13:13:40 +0000

    Menu page - structure

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,30 @@
 function Menu () {
     return (
-        <div>This is the Menu page.</div>
+        <div>
+            <h1>Our Menu</h1>
+            <div className="Brunch">
+                <h3>Brunch</h3>
+                <p>Sourdough toast with butter and jam/honey (£3.20)</p>
+                <p>2 eggs on sourdough toast (£5.90)</p>
+                <p>Bacon bap with scrambled eggs (£7.30)</p>
+                <p>Homemande granola + Greek yoghurt (£6.30)</p>
+                <p>3 fluffy pancakes with sweet or savoury toppings (£8.90)</p>
+            </div>
+            <div className="Lunch">
+                <h3>Lunch</h3>
+                <p>Lunch special - ask for today's option (£9.40)</p>
+                <p>Pan-fried halloumi + avocado + rocket baguette (£9.40)</p>
+                <p>Salad bowl - a mix of today's salads and green leaves (£7.70)</p>
+            </div>
+            <div className="Drinks">
+                <p>Espresso (£2.40)</p>
+                <p>Americano (£2.70)</p>
+                <p>Latte (£3.20)</p>
+                <p>Hot chocolate (£3.30)</p>
+                <p>Tea (£2.70)</p>
+                <p>Mineral water (£1.80)</p>
+            </div>
+        </div>
     )
 
 };
