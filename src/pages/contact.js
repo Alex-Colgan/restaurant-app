a085a02c55f33800322be41ab950b94266aaddbe
Alex-Colgan <alexandercolgan@gmail.com>
2022-10-31 13:45:38 +0000

    minor code formatting

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,11 @@
 import NavBar from "./nav";
-function Contact () {
-    return (
-        <div>
-            <NavBar />
-            <h1>CONTACT US</h1>
-            <h2>Please complete the form below.</h2>
-            
-        </div>
-    )
-
-};
- export default Contact;
+function Contact() {
+  return (
+    <div>
+      <NavBar />
+      <h1>CONTACT US</h1>
+      <h2>Please complete the form below.</h2>
+    </div>
+  );
+}
+export default Contact;
