69666206ce127b5738689301414003627e5206a4
zenlucia <zenarts2020@163.com>
2022-10-31 15:39:07 +0000

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,16 @@
-function Contact () {
+import NavBar from "./nav";
+function Contact() {
     return (
         <div>
+            <NavBar />
             <h1>CONTACT US</h1>
-            <h2>Please complete the form below.</h2>
-            
+            <h3>Opening Times</h3>
+            <ul>
+                <li>Mon – Fri: 8am – 8pm</li>
+                <li>Sat – Sun: 9am – 7pm</li>
+            </ul>
+            <h4>Tel. +44 161 881 5777</h4>
+            <h4>20 Hilton Street Northern Quarter, Manchester M1 1FR England</h4>
         </div>
     )
 
