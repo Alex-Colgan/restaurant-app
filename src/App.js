a78632bbba19918bf4c97f4681f7762dc096fda4
Alex-Colgan <alexandercolgan@gmail.com>
2022-10-31 11:14:27 +0000

    Restaurant app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Menu from './pages/menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hi.</h2>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="menu">Menu</a>
+          </li>
+        </ul>
+      </nav>
+      <BrowserRouter>
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/menu.js
@@ -0,0 +1,7 @@
+function Menu () {
+    return (
+        <div>This is the Menu page.</div>
+    )
+
+};
+ export default Menu;
